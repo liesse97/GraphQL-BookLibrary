57368f2f3655907b97a7105e4380d6bde54edf58
Liesse Ishimwe <ishimwelieish@gmail.com>
2021-03-25 09:53:20 +0100

    added

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 
-﻿
+
 //allow cross-origin request from front end
 //we install cors to allow requestes from other package like front-end 
 const cors = require('cors');
@@ -19,12 +19,12 @@ const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 
 // const uri="mongodb+srv://liesse:12345@cluster0.3o3fz.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const uri= "mongodb+srv://liesse:12345@cluster0.3o3fz.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const uri = "mongodb+srv://liesse:12345@cluster0.3o3fz.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
 //connect the mongodb database
-mongoose.connect(uri,{
-      useUnifiedTopology: true ,
-       useNewUrlParser: true
+mongoose.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 })
 //an event listen to a function once it open
 mongoose.connection.once('open', () => {
@@ -34,15 +34,16 @@ mongoose.connection.once('open', () => {
 
 // bind express with graphql
 //1. middleware, 2.route 3.handle the graphql
-app.use('/graphql',graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     //have the schema in the middleware to map the graph
-   //it is defining our graphql, the data that is going to be define in mongodb so that monogb will understad tje data
+    //it is defining our graphql, the data that is going to be define in mongodb so that monogb will understad tje data
     schema,
 
-   //we want to use the graphiql tools when we go to the(/graphql) in our browser
-   graphiql: true
+    //we want to use the graphiql tools when we go to the(/graphql) in our browser
+    graphiql: true
 }));
 
+//nodemon app and writ to your browser"http://localhost:5000/graphql?"
 app.listen(5000, () => {
     console.log('now listening for requests on port 5000');
 });
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,7 +4,6 @@
 //where we are going our scheme that is going to describe the data, allstå the relation between them and how we can reach into the graphl to interact(query,mutate)
 
 
-
 //we grab all the differen property from the graphql
 // and we define our first our first object type which is a BookType which has different fields which are wrap a function
 
@@ -18,94 +17,92 @@ const Book = require('../models/books');
 const Author = require('../models/Author');
 
 //calling lodash library
- //we install loadash, allow us different way to find data inside this array, change it and more
+//we install loadash, allow us different way to find data inside this array, change it and more
 const _ = require('lodash');
 
 
 
 //define object type we want like(books), grab from graphql
 //we are taking the variable type and storing it to qraphql so we can use it inside the file
-const{GraphQLObjectType,
-     GraphQLString,
-     GraphQLSchema,
+const { GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
     GraphQLID,
-  GraphQLInt,
-GraphQLList,
- GraphQLNonNull
- }= graphql;
+    GraphQLInt,
+    GraphQLList,
+    GraphQLNonNull
+} = graphql;
 
 // function taht takes an object that are going to define what this booktype is all about
 const BookGroup = new GraphQLObjectType({
     name: 'Book',
     //fields are going to be like a name property, a genre and id, important to have it like afunction so they can recognise each other
-    fields: ( ) => ({
+    fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
         //author is parent
-        author:{
-            type:AuthorGroup,
+        author: {
+            type: AuthorGroup,
             //use the resolve function to tell the graphql which auther correspend to the book
             //we have the data on the parent object that has pass throught to the resolve function
-             resolve(parent, args){
+            resolve(parent, args) {
                 //  console.log(parent)
                 //parent is the initial author
-               // return _.find(authors, { id: parent.authorId });
-                
-               //whenever user make a query for the books,then they can also request the author
-               return Author.findById(parent.authorId);
+                // return _.find(authors, { id: parent.authorId });
+
+                //whenever user make a query for the books,then they can also request the author
+                return Author.findById(parent.authorId);
 
 
             }
         }
 
     })
-    });
+});
 
-    const AuthorGroup = new GraphQLObjectType({
+const AuthorGroup = new GraphQLObjectType({
     name: 'author',
     //fields are going to be like a name property, a genre and id, important to have it like afunction so they can recognise each other
-    fields: ( ) => ({
+    fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         age: { type: GraphQLInt },
         //books is parent
-         books:{
-             //we want to list all books of an author not just one
-            type:new GraphQLList(BookGroup),
+        books: {
+            //we want to list all books of an author not just one
+            type: new GraphQLList(BookGroup),
 
-             resolve(parent, args){
+            resolve(parent, args) {
                 //  return all the books with the same auther Id
-                 console.log(parent.id)
-               // return _.filter(books, { authorId: parent.id });
+                console.log(parent.id)
+                // return _.filter(books, { authorId: parent.id });
 
-                 return Book.find({authorId: parent.id });
-
-                
+                return Book.find({ authorId: parent.id });
 
             }
         }
-        
+
     })
-    });
-    
-    const Mutate= new GraphQLObjectType({
+});
+
+const Mutate = new GraphQLObjectType({
     name: 'Mutation',
-    fields:{
+    fields: {
         //will allow us to add author,delet
-        AddAuthor: { 
+        AddAuthor: {
             type: AuthorGroup,
             //two argument we expect from the user when they are using the mutation add author
-            args:{
-            name: { type: new GraphQLNonNull(GraphQLString) },
-            age: { ype: new GraphQLNonNull(GraphQLInt) }
-        },
-        //lwhere we take the argument and make a new instant of the author and store it in our a database
-    
-        resolve(parent, args){
-            //Author from our model that we imported
-            //create a new instant in our database
-              //we created a locale variable called and send to a new locale variable of the database
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
+            },
+            //lwhere we take the argument and make a new instant of the author and store it in our a database
+
+            resolve(parent, args) {
+                //Author from our model that we imported
+                //create a new instant in our database
+                //we created a locale variable called and send to a new locale variable of the database
                 let author = new Author({
                     name: args.name,
                     age: args.age
@@ -115,16 +112,16 @@ const BookGroup = new GraphQLObjectType({
                 return author.save();
             }
         },
-        AddBook: { 
+        AddBook: {
             type: BookGroup,
             //two argument we expect from the user when they are using the mutation add author
-            args:{
-             name: { type: new GraphQLNonNull(GraphQLString) },
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
                 genre: { type: new GraphQLNonNull(GraphQLString) },
                 authorId: { type: new GraphQLNonNull(GraphQLID) }
-        },
-        //where we take the argument THE BOOKMODEL and make a new instant of the author and store it in our a database
-     resolve(parent, args){
+            },
+            //where we take the argument THE BOOKMODEL and make a new instant of the author and store it in our a database
+            resolve(parent, args) {
                 let book = new Book({
                     name: args.name,
                     genre: args.genre,
@@ -139,8 +136,8 @@ const BookGroup = new GraphQLObjectType({
 
 //3. define our RootEntryPoint which define how to jump in in to graph:different option on how to jump in the graph:1.book(bookType, argument:id in a string) 
 //efter we recieve it we are going to fire the resove fucntion that will help to grap the book from the database of some other source
-    //entrypoint into our data, define relationship between type
-    //define ripequery where we can jump it the graph and grab data
+//entrypoint into our data, define relationship between type
+//define ripequery where we can jump it the graph and grab data
 
 const RootEntryPoint = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -150,40 +147,40 @@ const RootEntryPoint = new GraphQLObjectType({
             type: BookGroup,
             // the user need to pass an id like book(id:123) and that's how we know what book to land in and return
             args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-               //use lodash to look through the book array to find the book that have an id equal to the(args:id)that the user send along and return it
-              // return _.find(books, { id: args.id });
-         return Book.findById(args.id);
+            resolve(parent, args) {
+                //use lodash to look through the book array to find the book that have an id equal to the(args:id)that the user send along and return it
+                // return _.find(books, { id: args.id });
+                return Book.findById(args.id);
 
 
             }
         },
-       auther: {
+        auther: {
             type: AuthorGroup,
             // the user need to pass an id like book(id:123) and that's how we know what book to land in and return
             args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-               //use lodash to look through the book array to find the book that have an id equal to the(args:id)that the user send along and return it
-               
-               //return _.find(authors, { id: args.id });
-               return Author.findById(args.id);
-                
+            resolve(parent, args) {
+                //use lodash to look through the book array to find the book that have an id equal to the(args:id)that the user send along and return it
+
+                //return _.find(authors, { id: args.id });
+                return Author.findById(args.id);
+
 
             }
         },
         //want all books
-         books: {
+        books: {
             type: new GraphQLList(BookGroup),
-            resolve(parent, args){
-               //return books
-               return Book.find({});
+            resolve(parent, args) {
+                //return books
+                return Book.find({});
             }
         },
         authors: {
             type: new GraphQLList(AuthorGroup),
-            resolve(parent, args){
-              // return authors
-            return Author.find({});
+            resolve(parent, args) {
+                // return authors
+                return Author.find({});
 
             }
         },
@@ -193,9 +190,9 @@ const RootEntryPoint = new GraphQLObjectType({
 
 //we are defining which query the user can use in the front end
 //export it so we can use as property to our app.use
-    //when exporting our graphqlschema we are saying that a user can query using rootQuery and mutation using mutation 
+//when exporting our graphqlschema we are saying that a user can query using rootQuery and mutation using mutation 
 
 module.exports = new GraphQLSchema({
     query: RootEntryPoint,
-    Mutation:Mutate
+    Mutation: Mutate
 });
